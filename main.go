51f03f5541b46de5da4626a1121272a139f2298e
CiroLong <1290363659@qq.com>
2025-05-19 02:03:59 +0800

    Init Program

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run(":8080") // 监听并在 0.0.0.0:8080 上启动服务
+}
